5ca28d631712b3067b0bad863e7f5123c0a5f272
Larry Wang <128409174+LarryWg@users.noreply.github.com>
2023-04-08 22:09:35 -0400

    Finished

--- GUI_Development/Integer_Verification.java
+++ GUI_Development/Integer_Verification.java
@@ -1,37 +1,72 @@
-import java.swing.*;
+import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
-class Interger_Verification extends JFrame implements ActionListener{
-    private JButton verifyButton;
-    private JTextField verifyTextField, cancelButton;
-    private JLabel verifyLabel;
+class Integer_Verification extends JFrame implements ActionListener{
+    private JTextField textField;
+    private JButton verifyButton, cancelButton;
+    private JLabel resultLabel;
 
-    public Interger_Verification(){
-        setTitle("Interger Verification");
+    public Integer_Verification(){
+        setTitle("Integer Verifier");
+        setSize(300,150);
+        setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
-        textField = new JTextField(10);
-        add(textField);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(5, 5, 5, 5);
+        panel.add(new JLabel("Enter an integer:"), c);
+
+        c = new GridBagConstraints();
+        c.gridx = 1;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 1.0;
+        c.insets = new Insets(5, 5, 5, 5);
+        textField = new JTextField(8);
+        panel.add(textField, c);
+
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 1;
+        c.insets = new Insets(5, 5, 5, 5);
         verifyButton = new JButton("Verify");
         verifyButton.addActionListener(this);
-        add(verifyButton);
+        panel.add(verifyButton, c);
+
+        c = new GridBagConstraints();
+        c.gridx = 1;
+        c.gridy = 1;
+        c.insets = new Insets(5, 5, 5, 5);
         cancelButton = new JButton("Cancel");
         cancelButton.addActionListener(this);
-        add(cancelButton);
+        panel.add(cancelButton, c);
+
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 2;
+        c.gridwidth = 2;
+        c.insets = new Insets(5, 5, 5, 5);
         resultLabel = new JLabel("");
-        add(resultLabel);
-        
-        pack();
+        panel.add(resultLabel, c);
+
+        getContentPane().add(panel);
+
+        setLocationRelativeTo(null);
         setVisible(true);
     }
     public void actionPerformed(ActionEvent e){
         if(e.getSource()==verifyButton){
             try{
-                int value = Interger.parseInt(textField.getText());
-                resultLabel.setText(value+" is an integer.");
+                int value = Integer.parseInt(textField.getText());
+                resultLabel.setText("\n"+value+"is an integer.");
             }catch(NumberFormatException ee){
-                resultLabel.setText(textField.getText()+" is not an integer.");
+                resultLabel.setText("\n"+textField.getText()+" is not an integer.");
             }
         } else if(e.getSource()==cancelButton){
             dispose();
@@ -39,7 +74,7 @@ class Interger_Verification extends JFrame implements ActionListener{
     }
     
     public static void main(String[] args){
-        new Interger_Verification();
+        new Integer_Verification();
     }
 }
 
